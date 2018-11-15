ccb04c4d9a4b4038b8a37887eaa1b9aa4cf8957b
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-11-15 18:16:48 +0100

    Function finalized

--- quatFromAngleDivided.m
+++ quatFromAngleDivided.m
@@ -1,5 +1,5 @@
 function [resultQuat] = quatFromAngleDivided (vector, angle)
 %The vector should have 3 components, and the angle is only accepted in
 %degrees
-resultQuat = [cosd(angle);sind(angle)*vector];
+resultQuat = [cosd(angle/2);sind(angle/2)*vector];
 end
