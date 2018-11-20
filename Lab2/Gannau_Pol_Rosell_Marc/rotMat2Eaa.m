d5a0aca7aa44f5e13a269083637fb4beaf66864a
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-11-20 11:46:22 +0100

    Function Well Working Now

--- Lab2/Gannau_Pol_Rosell_Marc/rotMat2Eaa.m
+++ Lab2/Gannau_Pol_Rosell_Marc/rotMat2Eaa.m
@@ -3,8 +3,8 @@ function [Axis,Angle] = rotMat2Eaa (R)
 %angle
 Axis = zeros(3,1);
 t = R(1,1) + R(2,2) + R(3,3);
-Angle = arcosd((t-1)/2);
-uX = ((R-R')/(2*sind(angle)));
+Angle = acosd((t-1)/2);
+uX = ((R-R')/(2*sind(Angle)));
 Axis(1) = uX(3,2);
 Axis(2) = uX(1,3);
 Axis(3) = uX(2,1);
