3bdf497d644b368d3a51f6dc97ae83cfb08c2b8c
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-11-20 11:33:01 +0100

    Another function

---
+++ Lab2/Gannau_Pol_Rosell_Marc/rotMat2Eaa.m
@@ -0,0 +1,11 @@
+function [Axis,Angle] = rotMat2Eaa (R)
+%Given a Rotation Matrix, the function returns the euler axis and the euler
+%angle
+Axis = zeros(3,1);
+t = R(1,1) + R(2,2) + R(3,3);
+Angle = arcosd((t-1)/2);
+uX = ((R-R')/(2*sind(angle)));
+Axis(1) = uX(3,2);
+Axis(2) = uX(1,3);
+Axis(3) = uX(2,1);
+end
