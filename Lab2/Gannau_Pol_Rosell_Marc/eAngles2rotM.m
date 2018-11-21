a2fc95716a775a3d4f270ecfbef5251eaab6a106
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-11-21 21:30:01 +0100

    More

--- Lab2/Gannau_Pol_Rosell_Marc/eAngles2rotM.m
+++ Lab2/Gannau_Pol_Rosell_Marc/eAngles2rotM.m
@@ -1,11 +1,11 @@
 function [result] = eAngles2rotM (phy, theta, psi)
-%Given 3 angles in degrees the function devolves a rotation matrix
-cpsi = cosd(psi);
-spsi = sind(psi);
-ctheta = cosd(theta);
-stheta = sind(theta);
-cphy = cosd(phy);
-sphy = sind(phy);
+%Given 3 angles in radians the function devolves a rotation matrix
+cpsi = cos(psi);
+spsi = sin(psi);
+ctheta = cos(theta);
+stheta = sin(theta);
+cphy = cos(phy);
+sphy = sin(phy);
 result = [ctheta*cpsi (cpsi*stheta*sphy)-(cphy*spsi) (cpsi*cphy*stheta)+(spsi*sphy);
             cphy*spsi (spsi*stheta*sphy)+(cphy*cpsi) (spsi*stheta*cphy)-(cpsi*sphy);
             -stheta ctheta*sphy ctheta*cphy];
