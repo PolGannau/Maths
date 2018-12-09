595c73ab4c83ff809a5a9a1a28db64fd31daf21a
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2018-12-09 22:15:19 +0100

    Exercise 3 finished

--- Lab3/Gannau_Pol_Rosell_Marc/multiplyQuat.m
+++ Lab3/Gannau_Pol_Rosell_Marc/multiplyQuat.m
@@ -2,8 +2,8 @@ function [resultQuat] = multiplyQuat(q,p)
 %q y p deben ser vecotres columna
 q0 = q(1);
 p0 = p(1);
-qVec = q(2:end);
-pVec = p(2:end);
+qVec = q(2:4);
+pVec = p(2:4);
 resultQuat = zeros(4,1);
 resultQuat(1) = q0*p0 - qVec'*pVec;
 resultQuat(2:end) = q0*pVec + p0*qVec + cross(qVec,pVec);
