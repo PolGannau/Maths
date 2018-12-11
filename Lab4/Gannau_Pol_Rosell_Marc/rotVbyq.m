4d1a8f4204c79e16598830f5f422983dd22d669c
Pol Gannau Coma <36154511+PolGannau@users.noreply.github.com>
2018-12-11 17:40:38 +0100

    tryng 2.1.2

---
+++ Lab4/Gannau_Pol_Rosell_Marc/rotVbyq.m
@@ -0,0 +1,5 @@
+function rotVbyq()
+%this function rotates a Vector by using Quaternion multiplications.
+%has to transforn the quaternion into a unit quaternion and then
+%multiplicate the vector (that was passed into a quaternion) and the result
+%is the quaternion W without the "0".
